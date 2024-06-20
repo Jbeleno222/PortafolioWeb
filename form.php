ffb8c44a63424c96f999874a9805f2f31ac5b072
Jbeleno222 <jjbelenoc@ufpso.edu.co>
2024-06-20 13:35:15 -0500

    nuevo portafolio

---
+++ form.php
@@ -0,0 +1,23 @@
+<?php
+
+$name = $_POST['name'];
+$mail = $_POST['email'];
+$subject = $_POST['subject'];
+$mensaje = $_POST['text'];
+
+$header .= "Content-Type: tect/plain";
+
+$contenido = "Este mensaje fue enviado por $name \r\n";
+$contenido .= "su e-mail es: $mail \r\n";
+$contenido .= "el asunto es: $subject \r\n";
+$contenido .= "mensaje: $text \r\n";
+
+
+$destinatario = 'jjbelenoc@ufpso.edu.co';
+$asunto = 'Este mail fue enviado desde: Portafolio Web';
+
+mail($destinatario, $asunto, utf8_decode($mensaje), $header);
+
+header('Location:html/exito.html');
+
+?>
---
+++ js/main.js
@@ -0,0 +1,20 @@
+const typed = new Typed('.typed', {
+    strings : [
+        
+        '<i class="titiulos" > Web Developer </i>',
+        '<i class="titiulos" > Software Developer </i>',
+        '<i class="titiulos" > Web Designer </i>',
+        '<i class="titiulos" > Photographer </i>'
+    ],
+    typeSpeed: 75,
+    starDelay: 300,
+    backSpeed: 90,
+    smartBackspace: true,
+    shuffle: false,
+    backDelay: 1300,
+    loop: true,
+    loopCount: false,
+    showCursor: true,
+    cursorChar: "|",
+    contentType: 'html',
+});
